80d8b5bf1b61a115eac80934be3d8144f59240c1
Keanne-Wesley <keannewesleye@gmail.com>
2025-04-01 21:20:08 +0800

    Delete backend directory

--- backend/models/Task.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const TaskSchema = new mongoose.Schema({
-    text: String,
-    completed: Boolean,
-});
-
-module.exports = mongoose.model("Task", TaskSchema);
